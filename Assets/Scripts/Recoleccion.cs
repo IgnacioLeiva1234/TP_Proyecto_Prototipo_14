8a0ef17676f11e5f654245ef96e227c1a3f6f78f
IgnacioLeiva1234 <49702813@est.ort.edu.ar>
2026-05-29 12:06:30 -0300

    entrega 2

---
+++ Assets/Scripts/Recoleccion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recoleccion : MonoBehaviour
+{
+     static int contador = 0;
+
+     void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            contador++;
+            Debug.Log("Objetos recolectados: " + contador);
+            Destroy(gameObject);
+        }
+    }
+}
