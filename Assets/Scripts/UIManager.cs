c26187929280ef7a4c9161d0b5a1f02032879136
IgnacioLeiva1234 <49702813@est.ort.edu.ar>
2026-06-05 10:41:31 -0300

    entrega 3 incompleto

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,6 +8,8 @@ public class UIManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI timerText;
+    public GameObject WinPanel;
+    public GameObject DefeatPanel;
 
     public void UpdateScore(int score)
     {
@@ -17,5 +19,14 @@ public class UIManager : MonoBehaviour
     public void UpdateTimer(float timer)
     {
         timerText.text = "Tiempo: " + Mathf.Ceil(timer);
+    }
+     public void MostrarPantallaWin()
+    {
+        WinPanel.SetActive(true);
+    }
+
+    public void MostrarPantallaGameOver()
+    {
+        DefeatPanel.SetActive(true);
     }
 }
